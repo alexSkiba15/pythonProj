e3cb992ddc453217a33321001f17d6bc4be61c6a
alexSkiba15 <alexee1598@gmail.com>
2019-11-15 00:59:49 +0200

    Hotfix

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from store import views
 
 urlpatterns = [
     url(r'^cars/$', views.cars_list),
-    url(r'^cars/view/car$', views.car_view),
+    url(r'^cars/view/car/$', views.car_view),
     url(r'^cars/view/car/(?P<car_id>[0-9]+)$', views.car_view),
     url(r'^cars/view/owner$', views.owner_view),
     url(r'^cars/view/owner/(?P<owner_id>[0-9]+)$', views.owner_view),
